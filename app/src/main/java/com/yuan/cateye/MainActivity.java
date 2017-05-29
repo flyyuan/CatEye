1a24f6fa06582bfe7a5188b336986be95eef33e3
BennyShi <740004544@qq.com>
2017-05-30 01:04:26 +0800

    再次初始化，使用Camera2

--- app/src/main/java/com/yuan/cateye/MainActivity.java
+++ app/src/main/java/com/yuan/cateye/MainActivity.java
@@ -1,23 +1,60 @@
 package com.yuan.cateye;
 
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
-import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    ///为了使照片竖直显示
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     private SurfaceView mSurfaceView;
     private SurfaceHolder mSurfaceHolder;
-    private Camera mCamera;
-    private ImageView iv_show;
-    private int viewWidth,viewHeight;//SurfaceView的宽高
+//    private ImageView iv_show;
+    private CameraManager mCameraManager;//摄像头管理器
+    private Handler childHandler,mianHandler;
+    private String mCameraID;//摄像头ID，0为后，1为前
+    private ImageReader mImageReader;
+    private CameraCaptureSession mCameraCaptureSession;
+    private CameraDevice mCameraDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,80 +64,174 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        iv_show = (ImageView) findViewById(R.id.iv_show_camera_activity);
-        //SurfaceView
+//        iv_show = (ImageView) findViewById(R.id.iv_show_camera_activity);
+        //mSurfaceView
         mSurfaceView = (SurfaceView) findViewById(R.id.surface_view_camera2_activity);
+        mSurfaceView.setOnClickListener(this);
         mSurfaceHolder = mSurfaceView.getHolder();
-        //mSurfaceView不需要自己的缓冲区
-        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mSurfaceHolder.setKeepScreenOn(true);
         //mSurfaceView添加回调
-        mSurfaceHolder.addCallback(new SurfaceHolder.Callback2() {
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                //初始化相机
-                initCamera();
+                //初始化Camera
+                initCamera2();
             }
 
             @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
             }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
                 //释放Camera资源
-                if (mCamera != null){
-                    mCamera.stopPreview();
-                    mCamera.release();
+                if (null != mCameraDevice) {
+                    mCameraDevice.close();
+                    MainActivity.this.mCameraDevice = null;
                 }
             }
-
-            @Override
-            public void surfaceRedrawNeeded(SurfaceHolder surfaceHolder) {
-            }
         });
-        mSurfaceView.setOnClickListener(this);
     }
 
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus){
-        super.onWindowFocusChanged(hasFocus);
-        if (mSurfaceView != null){
-            viewHeight = mSurfaceView.getHeight();
-            viewWidth = mSurfaceView.getWidth();
+    //初始化Camera2
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void initCamera2(){
+        HandlerThread handlerThread = new HandlerThread("Camera2");
+        handlerThread.start();
+        childHandler = new Handler(handlerThread.getLooper());
+        mianHandler = new Handler(Looper.getMainLooper());
+        mCameraID = ""+ CameraCharacteristics.LENS_FACING_FRONT;//后摄像头
+        mImageReader = ImageReader.newInstance(1080,1920, ImageFormat.JPEG,1);
+        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            //可以在这里处理拍照得到的临时照片 例如，写入本地
+            @Override
+            public void onImageAvailable(ImageReader imageReader) {
+//                mCameraDevice.close();
+//                mSurfaceView.setVisibility(View.GONE);
+//                iv_show.setVisibility(View.VISIBLE);
+                //拿到拍照的数据
+                Image image = imageReader.acquireNextImage();
+                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                byte[] bytes = new byte[buffer.remaining()];
+                buffer.get(bytes);//从缓冲区存入字节数值
+                image.close();
+                final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                if (bitmap != null){
+//                    iv_show.setImageBitmap(bitmap);
+                }
+            }
+        }, mianHandler);
+        //获取摄像头管理
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            //打开摄像头
+            mCameraManager.openCamera(mCameraID, stateCallback, mianHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
+    /**
+     * 摄像头创建监听
+     */
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {//打开摄像头
+            mCameraDevice = camera;
+            //开启预览
+            takePreview();
+        }
 
-    private void initCamera() {
-        mCamera = Camera.open();//默认开启后置摄像头
-        mCamera.setDisplayOrientation(90);//摄像头旋转为90度
-        if (mCamera != null){
-            try {
-                Camera.Parameters parameters = mCamera.getParameters();
-                //设置预览照片大小
-                parameters.setPreviewFpsRange(viewWidth,viewHeight);
-                //预览相机照片帧数;
-                parameters.setPreviewFpsRange(4,10);
-                //设置格式
-                parameters.setPictureFormat(ImageFormat.JPEG);
-                //设置质量
-                parameters.set("jpeg-quality", 90);
-                //设置大小
-                parameters.setPictureSize(viewWidth,viewHeight);
-                //通过SurfaceView显示预览
-                mCamera.setPreviewDisplay(mSurfaceHolder);
-                //开始预览
-                mCamera.startPreview();
-            } catch (IOException e) {
-                e.printStackTrace();
+        @Override
+        public void onDisconnected(CameraDevice camera) {//关闭摄像头
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                MainActivity.this.mCameraDevice = null;
             }
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {//发生错误
+            Toast.makeText(MainActivity.this, "摄像头开启失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    /**
+     * 开始预览
+     */
+    private void takePreview() {
+        try {
+            // 创建预览需要的CaptureRequest.Builder
+            final CaptureRequest.Builder previewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            // 将SurfaceView的surface作为CaptureRequest.Builder的目标
+            previewRequestBuilder.addTarget(mSurfaceHolder.getSurface());
+            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            mCameraDevice.createCaptureSession(Arrays.asList(mSurfaceHolder.getSurface(), mImageReader.getSurface()),
+                    new CameraCaptureSession.StateCallback() // ③
+            {
+                @Override
+                public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                    if (null == mCameraDevice) return;
+                    // 当摄像头已经准备好时，开始显示预览
+                    mCameraCaptureSession = cameraCaptureSession;
+                    try {
+                        // 自动对焦
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                        // 打开闪光灯
+                        previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                        // 显示预览
+                        CaptureRequest previewRequest = previewRequestBuilder.build();
+                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(MainActivity.this, "配置失败", Toast.LENGTH_SHORT).show();
+                }
+            }, childHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void onClick(View view) {
-
+        takePicture();
     }
-
+    /**
+     * 拍照
+     */
+    private void takePicture() {
+        if (mCameraDevice == null) return;
+        // 创建拍照需要的CaptureRequest.Builder
+        final CaptureRequest.Builder captureRequestBuilder;
+        try {
+            captureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            // 将imageReader的surface作为CaptureRequest.Builder的目标
+            captureRequestBuilder.addTarget(mImageReader.getSurface());
+            // 自动对焦
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            // 自动曝光
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+            // 获取手机方向
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            // 根据设备方向计算设置照片的方向
+            captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+            //拍照
+            CaptureRequest mCaptureRequest = captureRequestBuilder.build();
+            mCameraCaptureSession.capture(mCaptureRequest, null, childHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ app/src/main/java/com/yuan/cateye/youtu/Config.java
@@ -0,0 +1,10 @@
+package com.yuan.cateye.youtu;
+
+/**
+ * Created by qingliang on 16/10/31.
+ */
+public class Config {
+    public static final String APP_ID = "10084108";                                 // 替换APP_ID
+    public static final String SECRET_ID = "AKIDa5UlYIHQ4yiqslDaVL7lIhx7A3nVOBDD";  // 替换SECRET_ID
+    public static final String SECRET_KEY = "Sou4So7TWXaqBb3W2bEnOOk4jVEzrR8t";     // 替换SECRET_KEY
+}
